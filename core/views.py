194cad1b677b876def1c81875b88d716008f4770
sebastianroja <sebastian.android.norambuena@gmail.com>
2023-04-17 21:21:41 -0400

    ingreso inicial y editor perfil

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .form import *
 from .models import Producto
 from .models import Usuario
-
+from django.contrib.auth import get_user_model
 
 
 # Create your views here. aca se debe crear una nueva funcion para la nueva landig
@@ -14,6 +14,8 @@ class usuario:
 
 
 def index(request):
+    User = get_user_model()
+    print(User.objects.all())
     return render(request, 'core/index.html')
 
 def Contacto(request):
@@ -32,22 +34,26 @@ def productos_copy(request):
     return render(request, 'core/productos_copy.html')
 
 def Crearcuenta(request):
+    
     datos = {
         'form': ClientForm()
     }
     if request.method=='POST':
         formulario= ClientForm(request.POST)
         if formulario.is_valid:
+            User = get_user_model()
+            user = User.objects.create_user(email=formulario['Correo'].value(), password="foo",username="hola")
             formulario.save()
             datos['mensaje'] = "Guardado Correctamente"
             
     return render(request,'core/Crearcuenta.html',datos)
 
 
-def EditarPerfil(request, id):
-    
-    print (id) 
-    user = Usuario.objects.get(idUsuario=id)
+def EditarPerfil(request):
+    #traer usuario actual#
+    idUsuario=1
+    print (idUsuario) 
+    user = Usuario.objects.get(idUsuario=idUsuario)
     print(user)
     datos = {
         'form': ClientForm(instance=user)
